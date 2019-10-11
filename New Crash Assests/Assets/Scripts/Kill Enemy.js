9ffb9f63257e3e497512353318ff50f81aa8754d
Loaten <skystrikesback@gmail.com>
2019-10-11 07:56:23 -0500

    Changes
    Crigg can commit murder on bird guy now in test level

---
+++ New Crash Assests/Assets/Scripts/Kill Enemy.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+//This Script will allow Crash to kill enemies or destroy certain objects
+
+var Enemy : GameObject;
+var AfterKill : GameObject;
+
+function OnTriggerEnter(other : Collider)
+
+{
+ 	 if(other.gameObject.tag == "Spin")
+  		{
+  			//Destroy(object);
+   	 		Enemy.SetActive (false);
+    		AfterKill.SetActive (true);
+  		}
+}
