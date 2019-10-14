9b5aaa8ea5929f4e368616c3f8be8a5aa409957f
Loaten <skystrikesback@gmail.com>
2019-10-14 18:47:45 -0500

    Planty Added

---
+++ New Crash Assests/Assets/Scripts/Pressure Plate.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var ButtonUnpushed : GameObject;
+var ButtonPushed : GameObject;
+
+
+//When Crash Enters the Collider. It will activate the pushed Button
+function OnTriggerStay(other : Collider)
+	{
+  		if(other.gameObject.tag == "Player")
+  		{
+  			ButtonUnpushed.SetActive(false);
+  			ButtonPushed.SetActive(true);
+  		}
+  	}
+
+//When Crash leaves the Collider. It will deactivate the pushed button
+function OnTriggerExit(other : Collider)
+	{
+  		if(other.gameObject.tag == "Player")
+  		{
+  			ButtonUnpushed.SetActive(true);
+  			ButtonPushed.SetActive(false);
+  		}
+  	}
