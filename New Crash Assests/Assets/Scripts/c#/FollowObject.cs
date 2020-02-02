313ec387289edcffaa1d96668a190766319ff7cf
SamTheTotallyNormalPerson <40128566+SamTheTotallyNormalPerson@users.noreply.github.com>
2020-02-02 11:51:31 -0500

    Oney Salami

---
+++ New Crash Assests/Assets/Scripts/c#/FollowObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour {
+
+    public GameObject Target;
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "Player") {
+            transform.Translate(Target.transform.position * Time.deltaTime);
+        }
+    }
+}
